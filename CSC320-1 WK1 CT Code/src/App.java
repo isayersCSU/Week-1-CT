1783bb260534d37577bfb574064747f961ccfc3f
isayersCSU <isobella.sayers@csuglobal.edu>
2024-09-15 19:41:30 -0600

    Week 1 CT Assignment
    Files from VS Code for Week 1 CT

---
+++ CSC320-1 WK1 CT Code/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Last Name");
+        System.out.println("First Name");
+        System.out.println("Address");
+        System.out.println("City");
+        System.out.println("ZIP Code");
+    }
+}
